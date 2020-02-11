5fb12b3571c69b28934413b68d8256cecb3ac1c9
DongNH <hoaidongit5@gmail.com>
2020-02-11 09:50:16 +0700

    Add new project

---
+++ app/src/main/java/com/dongnh/autosoundmode/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dongnh.autosoundmode
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
